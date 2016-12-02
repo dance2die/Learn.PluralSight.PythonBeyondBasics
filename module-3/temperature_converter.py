b071af13667d9b5beea0f79ff1faa3069247c78f
dance2die <dontbothermewithspam@gmail.com>
2016-12-02 07:15:22 -0500

    Reimplemented temperature converter using instance methods

--- module-3/temperature_converter.py
+++ module-3/temperature_converter.py
@@ -1,4 +1,23 @@
 class TemperatureConverter:
+    _temperature = 0
+
+    @property
+    def temperature(self):
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, value):
+        self._temperature = value
+
+    def __init__(self, temperature):
+        self.temperature = temperature
+
+    def to_c(self):
+        return self.f_to_c(self.temperature)
+
+    def to_f(self):
+        return self.c_to_f(self.temperature)
+
     @staticmethod
     def c_to_f(celsius):
         return celsius * 9 / 5 + 32
